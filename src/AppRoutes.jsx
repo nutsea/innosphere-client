adcce2325ba74b0244da31a631308649b7ca491f
nutsea <batyrova.n@icloud.com>
2023-12-21 21:13:01 +0300

    first commit

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import About from "./pages/About";
+import Price from "./pages/Price";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/price" element={<Price />} />
         </Routes>
     );
 }
