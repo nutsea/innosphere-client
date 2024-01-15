e89377356b9c00a0ef6717659bd3c72aa2463743
nutsea <batyrova.n@icloud.com>
2024-01-15 22:08:01 +0300

    admin panel

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import About from "./pages/About";
 import Price from "./pages/Price";
+import Admin from "./pages/Admin";
 
 const AppRoutes = () => {
     return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
             <Route path="/" element={<Main />} />
             <Route path="/about" element={<About />} />
             <Route path="/price" element={<Price />} />
+            <Route path="/admin" element={<Admin />} />
         </Routes>
     );
 }
